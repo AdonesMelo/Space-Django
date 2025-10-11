d2203f810f2ee3bb414a46601df1631a9830e476
Adones Melo <adones.n.m@outlook.com>
2025-10-11 19:39:26 -0300

    Templates: renderizada os arquivos html

--- galeria/views.py
+++ galeria/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Função principal da aplicação
 def index(request):
-    return HttpResponse('<h1>Olá mundo!</h1><p>Bem vindo ao espaço</p>')
+    return render(request, 'index.html')
 
