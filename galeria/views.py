5abb0ce4e0041511a51e73d21550a2cc68f0da84
Adones Melo <adones.n.m@outlook.com>
2025-10-11 21:10:55 -0300

    Carregando o template:  alterar o visual

--- galeria/views.py
+++ galeria/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 # Função principal da aplicação
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'galeria/index.html')
 
