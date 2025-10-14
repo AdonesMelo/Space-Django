a1a98318e038efa9f57da599cfd03d659058cb3a
Adones Melo <adones.n.m@outlook.com>
2025-10-13 21:21:54 -0300

    Imagens com nomes dinâmicos

--- galeria/views.py
+++ galeria/views.py
@@ -2,7 +2,18 @@ from django.shortcuts import render
 
 # Função principal da aplicação
 def index(request):
-    return render(request, 'galeria/index.html')
+    dados = {
+        1: {
+            'nome': 'Nebulosa de Carina',
+            'legenda': 'webbtelecope.org / NASA / James Webb'
+        },
+        2: {
+            'nome': 'Galáxia NGC 1079',
+            'legenda': 'nasa.org / NASA / Hubble'
+        }
+    }
+
+    return render(request, 'galeria/index.html', {'cards': dados})
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
