e4d2ae8b8e0d92df640a0733c84baf2ad2b20b1b
Adones Melo <adones.n.m@outlook.com>
2025-10-15 15:52:02 -0300

    Incluindo categoria

--- galeria/models.py
+++ galeria/models.py
@@ -1,8 +1,16 @@
 from django.db import models
 
 class fotografia(models.Model):
+    OPCAO_CATEGORIA = [
+        ('NEBULOSA', 'Nebulosa'),
+        ('ESTRELA', 'Estrela'),
+        ('GALÁXIA', 'Galáxia'),
+        ('PLANETA', 'Planeta'),
+    ]
+
     nome = models.CharField(max_length=100, null=False, blank=False)
     legenda = models.CharField(max_length=150, null=False, blank=False)
+    categoria = models.CharField(max_length=150, choices=OPCAO_CATEGORIA, default='')
     decricao = models.TextField(null=False, blank=False)
     foto = models.CharField(max_length=100, null=False, blank=False)
 
