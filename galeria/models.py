c6e93759551276c6f3840761fda4004d96b3ec4e
Adones Melo <adones.n.m@outlook.com>
2025-10-17 11:27:22 -0300

    declara o campo descrição para acrecestar informações no site

--- galeria/models.py
+++ galeria/models.py
@@ -12,7 +12,7 @@ class fotografia(models.Model):
     nome = models.CharField(max_length=100, null=False, blank=False)
     legenda = models.CharField(max_length=150, null=False, blank=False)
     categoria = models.CharField(max_length=150, choices=OPCAO_CATEGORIA, default='')
-    decricao = models.TextField(null=False, blank=False)
+    descricao = models.TextField(null=False, blank=False)
     foto = models.ImageField(upload_to='foto/%Y/%m/%d', blank=True)
     publicada = models.BooleanField(default=False)
     data_fotografia = models.DateTimeField(default=datetime.now, blank=False)
