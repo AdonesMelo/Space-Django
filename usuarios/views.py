d1f8de318ee2932bf4e152b7e467f26a20d08492
Adones Melo <adones.n.m@outlook.com>
2025-10-17 18:03:55 -0300

    Criando formulários de login e cadastro de forma mais eficiente

--- usuarios/views.py
+++ usuarios/views.py
@@ -4,7 +4,7 @@ from usuarios.forms import LoginForms
 def login(request):
     form = LoginForms()
 
-    return render(request, 'usuarios/login.html', {'form': form})
+    return render(request, 'usuarios/login.html', {'form': form})   
 
 def cadastro(request):
     return render(request, 'usuarios/cadastro.html')
