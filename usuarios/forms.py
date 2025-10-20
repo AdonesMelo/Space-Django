3e9a1642a154d5d578c675fa4cf5ea95964fe2c0
Adones Melo <adones.n.m@outlook.com>
2025-10-19 21:16:23 -0300

    Mensagens de erro para validações

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -77,6 +77,16 @@ class CadastroForms(forms.Form):
         if nome:
             nome = nome.strip()
             if ' ' in nome:
-                raise forms.ValidationError('Não é posivel inserir espaço dentro do nome usuário!')
+                raise forms.ValidationError('Espaço não são permitidos nesse campo')
             else:
                 return nome
+    
+    def clean_senha_2(self):
+        senha_1 = self.cleaned_data.get('senha_1')
+        senha_2 = self.cleaned_data.get('senha_2')
+
+        if senha_1 and senha_2:
+            if senha_1 != senha_2:
+                raise forms.ValidationError('Senhas não são iguais!')
+            else:
+                return senha_2      
