068092892ecc461ceefa16ce34b2edfb5e4fe9e6
Adones Melo <adones.n.m@outlook.com>
2025-10-28 20:54:06 -0300

    Deletar fotografia

--- apps/galeria/views.py
+++ apps/galeria/views.py
@@ -59,5 +59,9 @@ def editar_imagem(request, foto_id):
 
     return render(request, 'galeria/editar_imagem.html', {'form': form, 'foto_id': foto_id})
 
-def deletar_imagem(request):
-    return render(request)
+def deletar_imagem(request, foto_id):
+    fotografia = Fotografia.objects.get(id=foto_id)
+    fotografia.delete()
+    messages.success(request, 'Fotografia deletada como sucesso!')
+    
+    return redirect('index')
